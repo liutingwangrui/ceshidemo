4941677dd5020db9b8ea7422e4ddd6259a061ef7
liutingwangrui <liutingwangrui@example.com>
2018-06-22 14:40:51 +0800

    iiii

---
+++ toast/src/main/java/libary/util/com/toast/PixelUtil.java
@@ -0,0 +1,96 @@
+package libary.util.com.toast;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.WindowManager;
+
+/**
+ * Created by Administrator on 2018/6/21 0021.
+ * 获取屏幕宽度、px与dp的互转
+ */
+
+public class PixelUtil {
+    /**
+     * 获取运行屏幕宽度
+     */
+    public static int getScreenWidth(Activity context) {
+        WindowManager wm = (WindowManager) context
+
+                .getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return outMetrics.widthPixels;
+    }
+
+    /**
+     * 获得屏幕宽度
+     *
+     * @param context
+     * @return
+     */
+    public static int getScreenHeight(Context context) {
+        WindowManager wm = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return outMetrics.heightPixels;
+    }
+
+    /**
+     * 获得状态栏的高度
+     *
+     * @param context
+     * @return
+     */
+    public static int getStatusHeight(Context context) {
+
+        int statusHeight = -1;
+        try {
+            Class<?> clazz = Class.forName("com.android.internal.R$dimen");
+            Object object = clazz.newInstance();
+            int height = Integer.parseInt(clazz.getField("status_bar_height")
+                    .get(object).toString());
+            statusHeight = context.getResources().getDimensionPixelSize(height);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return statusHeight;
+    }
+
+    /**
+     * 获取控件宽
+     */
+    public static int getWidth(View view) {
+        int w = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        int h = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        view.measure(w, h);
+        return (view.getMeasuredWidth());
+    }
+
+
+    /**
+     * DP转PX
+     *
+     * @param context
+     * @param dpValue
+     * @return
+     */
+    public static int dp2px(Activity context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * PX转DP
+     *
+     * @param context
+     * @param pxValue
+     * @return
+     */
+    public static int px2dp(Activity context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+}
